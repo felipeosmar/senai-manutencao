f71402cd4ae1c9068a132a2fd00143b1be35ac3d
karl <88538786+Karl-developer@users.noreply.github.com>
2022-10-14 12:12:03 -0300

    [FEAT] Add information about project;

--- index.php
+++ index.php
@@ -1,36 +1,54 @@
-
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Select CRUD pages</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <style type="text/css">
-        .page-header h2{
+        .page-header h2 {
             margin-top: 0;
         }
-        table tr td:last-child a{
+
+        table tr td:last-child a {
             margin-right: 5px;
         }
     </style>
 </head>
+
 <body>
-<fieldset>
-<center>
-<legend>AULA do CALVO GITHUB</legend>
-<div class="form-group">
-    <a href="entregador-index.php" class="btn btn-primary" role="button">entregador</a> 
-	
-    	<a href="estabelecimento-index.php" class="btn btn-primary" role="button">estabelecimento</a>
-		<a href="item-index.php" class="btn btn-primary" role="button">item</a>
-		<a href="pedido-index.php" class="btn btn-primary" role="button">pedido</a>
-		<a href="produto-index.php" class="btn btn-primary" role="button">produto</a>
-		<a href="usuario-index.php" class="btn btn-primary" role="button">usuario</a>
-	<!-- TABLE_BUTTONS -->
-</div>
-</center>
-</fieldset>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <fieldset>
+        <center>
+            <div>
+                <h1>AULA do CALVO GITHUB</h1>
+                <h2>Esta aula é destinada para entender como o mengão vai descobrir
+                    as geleiras do rio de janeiro com a ajuda do thiaguinho </h2>
+                <p>Para entendermos o objetivo do porque o faustão está no sbt,
+                    precisamos entender primeiro porque a linguagem de programação python é utilizada para manipular
+                    a empresa de massas culinárias Barrila, se entendermos isso, iremos compreender finalmente que suponhamos
+                    que amanhã terá o dia do silvio bagos onde tata waterneck irá realizar o grande J em prol da sociedade socket_addrinfo_lookupa qual conviemos
+                    nos dias antigos de hoje, creio que este seja o mitov pelo qual o vasco estará na série A e não na nba como havia predito Steve Carrel,
+                    inclusve gostaria de dizer que o fato de desenvolver sistemas é sus é por causa de toda a influência em que os influenciadores
+                    influenciam por meio da tecnologia a qual tecnologizou nossos parâmetros em formas de argumentos.
+
+                    boa noi te.
+                </p>
+            </div>
+
+
+            <div class="form-group">
+                <a href="entregador-index.php" class="btn btn-primary" role="button">entregador</a>
+                <a href="estabelecimento-index.php" class="btn btn-primary" role="button">estabelecimento</a>
+                <a href="item-index.php" class="btn btn-primary" role="button">item</a>
+                <a href="pedido-index.php" class="btn btn-primary" role="button">pedido</a>
+                <a href="produto-index.php" class="btn btn-primary" role="button">produto</a>
+                <a href="usuario-index.php" class="btn btn-primary" role="button">usuario</a>
+                <!-- TABLE_BUTTONS -->
+            </div>
+        </center>
+    </fieldset>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
+
 </html>
