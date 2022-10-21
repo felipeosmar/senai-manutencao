0ecfcb6da8ecdeef185fe526bfb03db895af2c3b
unknown <Administrador@SN-357591.edu.fiesc.com.br>
2022-10-21 11:24:51 -0300

    campanha eleitoral pro bolsonaro

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
 		<a href="usuario-index.php" class="btn btn-primary" role="button">usuario</a>
 	<!-- TABLE_BUTTONS -->
 </div>
+<img src="https://ogimg.infoglobo.com.br/politica/25445336-682-fda/FT1086A/760/WhatsApp-Image-2022-03-23-at-17.07.58.jpeg.jpg" alt="">
 </center>
 </fieldset>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
